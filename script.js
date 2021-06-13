ed1f38e865789d5273e43f8721bd45cb52f1f6ad
KashishGoyal905 <kashishgoyal961@gmail.com>
2021-06-13 09:31:34 +0530

    done with backend

--- script.js
+++ script.js
@@ -11,16 +11,17 @@ let isGameOver = false;
 
 function playerOneClick() {
     if(!isGameOver){
-    if(score!=winning){
-    score +=1;
-    z1.textContent = score;
-    if(score===winning){
+        if(score!=winning){
+        score +=1;
+        z1.textContent = score;
+        }
+        if(score===winning){
         isGameOver=true;
         console.log('true')
-    }  
+        z1.classList.add('winner');
+        z2.classList.add('loser');
+        }      
     } 
-    
-} 
 }
 
 
@@ -29,11 +30,13 @@ function playerTwoClick() {
     if(score2!=winning){
     score2 +=1;
     z2.textContent = score2;
+    }
     if(score2===winning){
     isGameOver=true;
     console.log('true');
-    }
-    }   
+    z2.classList.add('winner');
+    z1.classList.add('loser');
+    } 
 }
 }
 
@@ -44,10 +47,15 @@ function playerThreeClick() {
     z1.textContent = score;
     z2.textContent = score2;
     isGameOver=false;
+    z1.classList.remove('winner');
+    z1.classList.remove('loser');
+    z2.classList.remove('winner');
+    z2.classList.remove('loser');
+
 }
 
 choose.addEventListener('change', function(){
-    winning =choose.value;
-    console.log('winning')
+    winning =parseInt(choose.value);
+    console.log(`${winning}`);
 });
 
